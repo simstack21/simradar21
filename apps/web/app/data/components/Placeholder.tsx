79bc1561ce3c74f1e56ae7384f8cf7b0ebc71da1
Sebastian Krüll <51778048+sebastiankrll@users.noreply.github.com>
2026-03-07 19:58:29 +0100

    Fix discord and github links
    Closes #150

--- apps/web/app/data/components/Placeholder.tsx
+++ apps/web/app/data/components/Placeholder.tsx
@@ -9,7 +9,7 @@ export function Placeholder({ text }: { text: string }) {
 			<span className="font-bold text-2xl text-center">Work in progress... Stay tuned!</span>
 			<span className="text-center max-w-3xl">{text}</span>
 			<span className="text-center max-w-3xl">For now you can simply use the Search functionality to browse available data.</span>
-			<a href="https://discord.gg/eVvKBfUr" target="_blank" rel="noopener noreferrer">
+			<a href="https://discord.gg/7sS4kUcP53" target="_blank" rel="noopener noreferrer">
 				<Button
 					size="lg"
 					className="bg-transparent bg-linear-to-r from-primary via-primary/10 to-primary bg-size-[200%_auto] text-white hover:bg-transparent hover:bg-position-[99%_center] focus-visible:ring-primary dark:from-primary dark:via-primary/75 dark:to-primary dark:focus-visible:ring-primary/50b"
